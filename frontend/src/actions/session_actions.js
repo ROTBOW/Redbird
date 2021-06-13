c4c291b04d6acc233c92b7ccfe1c0070a35b0e1a
Josiah Leon <sjosiahleon@gmail.com>
2021-06-13 14:29:47 -0700

    frontend

---
+++ frontend/src/actions/session_actions.js
@@ -0,0 +1,14 @@
+export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+
+export const logoutUser = () => ({
+    type: RECEIVE_USER_LOGOUT
+});
+
+export const logout = () => dispatch => {
+    // Remove the token from local storage
+    localStorage.removeItem('jwtToken')
+    // Remove the token from the common axios header
+    APIUtil.setAuthToken(false)
+    // Dispatch a logout action
+    dispatch(logoutUser())
+};
