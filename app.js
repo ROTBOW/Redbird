70187380cc0e2115ae93da404f487fe68caaac0f
Josiah Leon <sjosiahleon@gmail.com>
2021-06-13 14:30:47 -0700

    more frontend

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const users = require('./routes/api/users');
-const tweets = require('./routes/api/tweets');
+const express = require("express");
 const app = express();
-const db = require('./config/keys.js').mongoURI
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 mongoose
     .connect(db, {useNewUrlParser: true})
     .then(() => console.log("Connected to the MongoDB worked"))
     .catch(err => console.log(err))
-const port = process.env.PORT || 5000;
 
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
+    
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/api/users', users)
 app.use('/api/tweets', tweets)
-app.get('/', (req, res) => res.send('boi, whats up?'));
 
-app.listen(port, () => console.log(`Server is running on port: ${port}`));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
