58e8915fea7c3a206fd48096fcdde2bf91fca23f
Josiah Leon <sjosiahleon@gmail.com>
2021-06-13 15:05:27 -0700

    almoost done

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,18 @@
+  
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>A Twitter Clone</p>
+        <footer>
+          Copyright &copy; 2021 Redbird
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
