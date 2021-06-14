abf22c068fc05005bc6d9c9d2c6bd69df8fef397
Josiah Leon <sjosiahleon@gmail.com>
2021-06-13 20:15:48 -0700

    im so freaking tired

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -27,3 +28,9 @@ app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
